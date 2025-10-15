0a4f69a7c8662cd3d0fecd90710b19ae048b1445
Daniil.Diveev <Daniil.Diveev@x5.ru>
2025-10-16 01:07:09 +0300

    feat: add invalid lint stuff;

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,7 +3,8 @@ import { Home } from '../pages/Home';
 export default function App() {
   return (
     <div style={{ maxWidth: 720, margin: '2rem auto', fontFamily: 'system-ui' }}>
-      <h1>Mock Frontend</h1>
+           <h1>Mock Frontend</h1>
+
       <Home />
     </div>
   );
