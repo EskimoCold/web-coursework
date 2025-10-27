5c8cebc4a0a7df6ed3867b2563b2c8c852c32b33
Demid0 <demid04ka@mail.ru>
2025-10-27 15:50:13 +0300

    feat: categories page

---
+++ frontend/src/contexts/CategoriesContext.tsx
@@ -0,0 +1,47 @@
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+
+export type Category = {
+  id: number,
+  name: string,
+  type: number,
+  icon: string,
+  description: string,
+};
+
+type CategoryContextType = {
+  categories: Category[];
+  setCategories: React.Dispatch<React.SetStateAction<Category[]>>;
+};
+
+const CategoryContext = createContext<CategoryContextType | undefined>(undefined);
+
+export const CategoryProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch('/api/cat/list');
+        if (!response.ok) throw new Error('Ошибка загрузки');
+        const data: Category[] = await response.json();
+        setCategories(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  return (
+    <CategoryContext.Provider value={{ categories, setCategories }}> {/* Добавлено setCategories */}
+      {children}
+    </CategoryContext.Provider>
+  );
+};
+
+export const useCategories = () => {
+  const context = useContext(CategoryContext);
+  if (!context) throw new Error('useCategories must be used within CategoryProvider');
+  return context;
+};
---
+++ frontend/src/pages/categories/CategoryCard.tsx
@@ -0,0 +1,20 @@
+import './categories.css';
+import { Icon } from '../../components/Icon';
+import { Category } from '../../contexts/CategoriesContext';
+
+type Props = {
+  cat: Category;
+  handleClick: () => void;
+  icon?: string;
+}
+
+export const CategoryCard: React.FC<Props> = ({cat, handleClick}: Props) => {
+
+  return (<div className='cat-card' onClick={handleClick}>
+    <div>
+      <p className='cat-card-title'>{cat.name}</p>
+      <p className={`cat-card-type-title ${(cat.type ? 'green' : 'red')}`}>{cat.type ? 'Доход' : 'Расход'}</p>
+    </div>
+    <Icon source={cat.icon} size={60} className={'cat-card-icon'}/>
+  </div>);
+};
